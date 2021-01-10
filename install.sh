47821319fe495e801c3c0098ae2791cdf13531f5
Paweł Suwiński <dracono@wp.pl>
2021-01-10 22:12:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
-wget -q -O snow.sh https://raw.githubusercontent.com/PawelSuwinski/snowMonit/main/snow.sh
+[[ -z $BINDIR ]] && BINDIR=$HOME
+
+echo 'Installing snow.sh parser...'
+wget -q -O "$BINDIR/snow.sh" https://raw.githubusercontent.com/PawelSuwinski/snowMonit/main/snow.sh
 chmod a+x snow.sh
 
+echo 'Adding Termux Widget shortcut...'
 [[ ! -d .shortcuts ]] && mkdir .shortcuts
 cat <<EOT > .shortcuts/snow
 #!/data/data/com.termux/files/usr/bin/bash
-bash ~/snow.sh -m 10
+bash "$BINDIR/snow.sh" -m 10
 read
 EOT
 chmod a+x .shortcuts/snow
+
+echo Done.
