956097929e60fc7adc0435a32d2e43f3e5936897
Paweł Suwiński <dracono@wp.pl>
2021-01-10 20:23:56 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+wget -q -O snow.sh https://raw.githubusercontent.com/PawelSuwinski/snowMonit/main/snow.sh
+chmod a+x snow.sh
+
+[[ ! -d . shortcuts ]] && mkdir .shortcuts
+cat <<<EOT > .shortcuts/snow
+#!/data/data/com.termux/files/usr/bin/bash
+bash ~/snow.sh -m 10
+read
+EOT
+chmod a+x .shortcuts/snow
