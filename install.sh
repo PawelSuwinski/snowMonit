1059bfd9f4df1ee4036d7755b2dbd17f776f3854
Paweł Suwiński <dracono@wp.pl>
2021-01-10 21:57:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ wget -q -O snow.sh https://raw.githubusercontent.com/PawelSuwinski/snowMonit/mai
 chmod a+x snow.sh
 
 [[ ! -d .shortcuts ]] && mkdir .shortcuts
-cat<<<EOT > .shortcuts/snow
+cat <<EOT > .shortcuts/snow
 #!/data/data/com.termux/files/usr/bin/bash
 bash ~/snow.sh -m 10
 read
